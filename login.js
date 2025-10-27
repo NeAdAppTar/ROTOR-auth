ee109f4d2811d5efd84cbd0503a0e2930b2cbe9b
NeAdAppTar <TIvan0931@yandex.ru>
2025-10-27 12:41:06 +0300

    auth

--- login.js
+++ login.js
@@ -4,8 +4,9 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
   const login = document.getElementById('login').value.trim();
   if (!login) return alert('Введите логин');
 
-  // cookie
   document.cookie = `userLogin=${encodeURIComponent(login)}; path=/; domain=.rotorbus.ru; max-age=${60 * 60 * 24 * 7}`;
+
+  localStorage.setItem('username', login);
 
   const params = new URLSearchParams(window.location.search);
   const redirect = params.get('redirect') || 'https://rotorbus.ru/employee_dashboard.html';
